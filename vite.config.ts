be4243d8dbeb925a8fd5718c545cefc21f13cfeb
krd2ad <43148592+krd2ad@users.noreply.github.com>
2025-08-03 20:25:32 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
-    port: 8080,
+    base: '/quiet-club-focus/',
   },
   plugins: [
     react(),
